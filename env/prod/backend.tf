e5726d0b1e2ac089ebe6c93455faca8d69713c6b
rajatbhawsar <rajatbhawsar@KTRAININD.COM>
2026-05-07 18:17:27 +0530

    updated backend

--- env/prod/backend.tf
+++ env/prod/backend.tf
@@ -5,8 +5,8 @@ terraform {
     # client_id            = ""     # Can also be set via `ARM_CLIENT_ID` environment variable.
     # client_secret        = "" # Can also be set via `ARM_CLIENT_SECRET` environment variable.
     storage_account_name = "stgaccounttfstateprod001" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                 # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    container_name       = "tfstate"                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate"    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
